15d2feb00820e75a5b51b74ae56b6838b214befc
unknown <rautanokka@gmail.com>
2022-01-18 21:02:05 +0200

    Tehtävä tehty

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [product, setProduct] = useState("")
+  const [data, setData] = useState([])
+
+  const addItem = () => {
+    setData(data.concat(product))
+    setProduct("")
+  }
+
+  const clearItems = () => {
+    setData([])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.textfield} onChangeText={(text) => setProduct(text)} value={product} />
+      <View style={styles.buttoncontainer}>
+        <Button title="ADD" onPress={() => addItem()} />
+        <Button title="CLEAR" onPress={() => clearItems()}/>
+      </View>
+      <Text style={styles.shoppinglistheader}>Shopping List</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) =>
+          <Text style={styles.itemtext}>{item}</Text>
+        }
+        keyExtractor={(item, index) => index.toString()} />
     </View>
   );
 }
@@ -17,4 +40,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttoncontainer: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    width: 150,
+    paddingTop: 10
+  },
+  shoppinglistheader: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 20,
+    paddingTop: 20
+  },
+  textfield: {
+    marginTop: 50,
+    width: 250,
+    borderWidth: 1,
+    borderColor: "grey"
+  },
+  itemtext: {
+    fontSize: 18
+  }
 });
